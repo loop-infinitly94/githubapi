f632db68edbe3a70b56573776588e871ac01376c
Shyam Kumar <shyam.kumar@lisec.com>
2021-03-23 10:30:47 +0400

    '+css-commenting'

--- src/component/displaylist.js
+++ src/component/displaylist.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import './list.css'
+import './list.css';
+import UserIcon  from '../assets/images/githubicon.png';
+
 function DisplayList(props) {
     /*
         get the firstKey 
     */
-    var gistKey = Object.keys(props.userGistList[0])[0];
-    // var currentFork = props.perGistsFork.length > 0 && props.perGistsFork.filter((eachFork) => true).slice(0, 3)
+    let gistKey = Object.keys(props.userGistList[0])[0];
     return (
         <div className="MainContainer">
-             <div className="grid-item">
+            <div className="grid-item">
                 Username
-            </div> 
+            </div>
             <div className="grid-item">
                 FileTypes/List
-            </div> 
+            </div>
             <div className="grid-item">
                 Fork Details
             </div>
             <div className="grid-item userNameLeft">
-                <img alt = {"userImage"} src={props.userGistList[0][gistKey].profileImg} className="userGistImage" />
+                <img alt={"userImage"} src={props.userGistList[0][gistKey].profileImg === "" ? UserIcon : props.userGistList[0][gistKey].profileImg} className="userGistImage" />
                 <span className="userName">{props.userGistList[0][gistKey].userName}</span>
             </div>
             <div className="grid-item fileNameMiddle">
                 {
                     props.userGistList.length > 0 && props.userGistList.map((eachItem, idx) => {
-                        var key = Object.keys(eachItem)[0]                        
+                        let key = Object.keys(eachItem)[0]
                         return (
                             Object.keys(eachItem[key].files).map((eachFile) => {
-                                var eachFileType = eachItem[key].files[eachFile].language
-                                var eachFileName = eachItem[key].files[eachFile].filename
+                                let eachFileType = eachItem[key].files[eachFile].language
+                                let eachFileName = eachItem[key].files[eachFile].filename
                                 return (
                                     <div className="fileSection">
                                         <span className="eachFileType">{eachFileType}</span>
@@ -42,17 +43,24 @@ function DisplayList(props) {
             </div>
             <div className="grid-item forkNameLast">
                 {
-                    props.perGistsFork.length > 0 && props.perGistsFork.map((eachFork, idx) => {
-                        var key = Object.keys(eachFork)[0]
-                        var eachGistForDetails = eachFork[key]
-                        eachGistForDetails.length > 0 && eachGistForDetails.map((data) => {
-                            return eachFork[key].length > 0 && <div className="grid-item">
-                            <span className="userName">{data.owner.login}</span>
+                    props.perGistsFork.length > 0 && props.perGistsFork.map((eachFork) => {
+                        let key = Object.keys(eachFork)[0]
+                        let eachGistForDetails = eachFork[key]
+                        return (
+                            <div className="forList">
+                                {eachGistForDetails.length > 0 ? <span className="userName">{key}</span> : null}
+                                {
+                                    eachGistForDetails.length > 0 && eachGistForDetails.map((data, idx) => {
+                                        return <div className="eachFork">
 
-                            <img alt = "userimage" src={data.owner.avatar_url} className="userGistImage" />
-                            <span className="userName">{data.owner.login}</span>
-                        </div>
-                        })
+                                            <img alt="userimage" src={data.owner.avatar_url === "" ? UserIcon : data.owner.avatar_url} className="userGistImage" />
+                                            <span className="userName">{data.owner.login}</span>
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        )
+
                     })
                 }
             </div>
--- src/component/main.js
+++ src/component/main.js
@@ -3,10 +3,58 @@ import SearchAndFilterData from './searchandfilterdata';
 import DisplayList from './displaylist';
 import { GET_UsersGists, GET_UserForks } from "../api/gistsapi";
 
-
+/*
+    temporary forkData
+*/
 const forkListttt = [
     {
-        "4815cd16092666cf2f360694d116eb6c": {
+        "4815cd16092666cf2f360694d116eb6c": [{
+            "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
+            "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
+            "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
+            "id": "aa5a315d61ae9438b18d",
+            "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
+            "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+            "files": {
+                "hello_world.rb": {
+                    "filename": "hello_world.rb",
+                    "type": "application/x-ruby",
+                    "language": "Ruby",
+                    "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
+                    "size": 167
+                }
+            },
+            "public": true,
+            "created_at": "2010-04-14T02:15:15Z",
+            "updated_at": "2011-06-20T11:34:15Z",
+            "description": "Hello World Examples",
+            "comments": 1,
+            "user": null,
+            "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+            "owner": {
+                "login": "shyam",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        },
+        {
             "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
             "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
             "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
@@ -35,7 +83,7 @@ const forkListttt = [
                 "login": "octocat",
                 "id": 1,
                 "node_id": "MDQ6VXNlcjE=",
-                "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
                 "gravatar_id": "",
                 "url": "https://api.github.com/users/octocat",
                 "html_url": "https://github.com/octocat",
@@ -51,7 +99,194 @@ const forkListttt = [
                 "type": "User",
                 "site_admin": false
             }
-        }
+        },
+        {
+            "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
+            "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
+            "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
+            "id": "aa5a315d61ae9438b18d",
+            "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
+            "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+            "files": {
+                "hello_world.rb": {
+                    "filename": "hello_world.rb",
+                    "type": "application/x-ruby",
+                    "language": "Ruby",
+                    "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
+                    "size": 167
+                }
+            },
+            "public": true,
+            "created_at": "2010-04-14T02:15:15Z",
+            "updated_at": "2011-06-20T11:34:15Z",
+            "description": "Hello World Examples",
+            "comments": 1,
+            "user": null,
+            "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+            "owner": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        }],
+
+    },
+    {
+        "918f9bbe2faa32135045407e1c032d94": [{
+            "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
+            "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
+            "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
+            "id": "aa5a315d61ae9438b18d",
+            "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
+            "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+            "files": {
+                "hello_world.rb": {
+                    "filename": "hello_world.rb",
+                    "type": "application/x-ruby",
+                    "language": "Ruby",
+                    "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
+                    "size": 167
+                }
+            },
+            "public": true,
+            "created_at": "2010-04-14T02:15:15Z",
+            "updated_at": "2011-06-20T11:34:15Z",
+            "description": "Hello World Examples",
+            "comments": 1,
+            "user": null,
+            "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+            "owner": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        },
+        {
+            "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
+            "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
+            "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
+            "id": "aa5a315d61ae9438b18d",
+            "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
+            "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+            "files": {
+                "hello_world.rb": {
+                    "filename": "hello_world.rb",
+                    "type": "application/x-ruby",
+                    "language": "Ruby",
+                    "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
+                    "size": 167
+                }
+            },
+            "public": true,
+            "created_at": "2010-04-14T02:15:15Z",
+            "updated_at": "2011-06-20T11:34:15Z",
+            "description": "Hello World Examples",
+            "comments": 1,
+            "user": null,
+            "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+            "owner": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        },
+        {
+            "url": "https://api.github.com/gists/aa5a315d61ae9438b18d",
+            "forks_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/forks",
+            "commits_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/commits",
+            "id": "aa5a315d61ae9438b18d",
+            "node_id": "MDQ6R2lzdGFhNWEzMTVkNjFhZTk0MzhiMThk",
+            "git_pull_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "git_push_url": "https://gist.github.com/aa5a315d61ae9438b18d.git",
+            "html_url": "https://gist.github.com/aa5a315d61ae9438b18d",
+            "files": {
+                "hello_world.rb": {
+                    "filename": "hello_world.rb",
+                    "type": "application/x-ruby",
+                    "language": "Ruby",
+                    "raw_url": "https://gist.githubusercontent.com/octocat/6cad326836d38bd3a7ae/raw/db9c55113504e46fa076e7df3a04ce592e2e86d8/hello_world.rb",
+                    "size": 167
+                }
+            },
+            "public": true,
+            "created_at": "2010-04-14T02:15:15Z",
+            "updated_at": "2011-06-20T11:34:15Z",
+            "description": "Hello World Examples",
+            "comments": 1,
+            "user": null,
+            "comments_url": "https://api.github.com/gists/aa5a315d61ae9438b18d/comments/",
+            "owner": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/21107?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+            }
+        }]
     }
 ]
 
@@ -70,7 +305,6 @@ class Main extends Component {
     }
 
     callBackUserGistList(responseData) {
-        console.log(responseData)
         if (responseData.status === 200) {
             this.setState({ userGistList: responseData.data })
             const mappedUserGist = this.mapUserGist()
@@ -80,6 +314,10 @@ class Main extends Component {
             console.warn(responseData.status + responseData.statusText);
         }
     }
+
+    /*
+        create an object that maps the gist ids with  forks
+    */
 
     mapUserGist() {
         const { userGistList } = this.state;
@@ -97,6 +335,10 @@ class Main extends Component {
         })
         return mappedUserGist;
     }
+
+    /*
+        serialized request for forks for each each gists
+    */
 
     async getUserForks(mappedUserGist) {
         const getAllUsersForks = new Promise((resolve, reject) => {
@@ -117,7 +359,6 @@ class Main extends Component {
         })
 
         const gistForks = await getAllUsersForks;
-        // console.log(gistForks)
         this.setState({
             perGistsFork: gistForks
         })
