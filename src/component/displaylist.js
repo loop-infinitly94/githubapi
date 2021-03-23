118563d2a852afab6f68024f6336c0ad8149d894
Shyam Kumar <shyam.kumar@lisec.com>
2021-03-23 10:34:41 +0400

    '+css-commenting'

--- src/component/displaylist.js
+++ src/component/displaylist.js
@@ -6,7 +6,7 @@ function DisplayList(props) {
     /*
         get the firstKey 
     */
-    let gistKey = Object.keys(props.userGistList[0])[0];
+    var gistKey = Object.keys(props.userGistList[0])[0];
     return (
         <div className="MainContainer">
             <div className="grid-item">
@@ -25,11 +25,11 @@ function DisplayList(props) {
             <div className="grid-item fileNameMiddle">
                 {
                     props.userGistList.length > 0 && props.userGistList.map((eachItem, idx) => {
-                        let key = Object.keys(eachItem)[0]
+                        var key = Object.keys(eachItem)[0]
                         return (
                             Object.keys(eachItem[key].files).map((eachFile) => {
-                                let eachFileType = eachItem[key].files[eachFile].language
-                                let eachFileName = eachItem[key].files[eachFile].filename
+                                var eachFileType = eachItem[key].files[eachFile].language
+                                var eachFileName = eachItem[key].files[eachFile].filename
                                 return (
                                     <div className="fileSection">
                                         <span className="eachFileType">{eachFileType}</span>
@@ -44,8 +44,8 @@ function DisplayList(props) {
             <div className="grid-item forkNameLast">
                 {
                     props.perGistsFork.length > 0 && props.perGistsFork.map((eachFork) => {
-                        let key = Object.keys(eachFork)[0]
-                        let eachGistForDetails = eachFork[key]
+                        var key = Object.keys(eachFork)[0]
+                        var eachGistForDetails = eachFork[key]
                         return (
                             <div className="forList">
                                 {eachGistForDetails.length > 0 ? <span className="userName">{key}</span> : null}
