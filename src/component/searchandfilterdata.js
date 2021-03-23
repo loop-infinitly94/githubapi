b2d8fb593ea60cd32d65d9f05e77f6b48f7c2d02
Shyam Kumar <shyam.kumar@lisec.com>
2021-03-23 11:31:45 +0400

    '+loader'

--- src/component/searchandfilterdata.js
+++ src/component/searchandfilterdata.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 class SearchAndFilterData extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             userName: ""
         }
     }
 
-    onSearchClicked(ev){
-        if(this.state.userName === ""){
+    onSearchClicked(ev) {
+        if (this.state.userName === "") {
             return
         }
         this.props.onSearchClickedProp(this.state.userName)
     }
 
-    onSearchValueChanged(ev){
-        this.setState({userName: ev.target.value})
+    onKeyPress(ev) {
+        if (ev.which === 13) {
+            this.props.onSearchClickedProp(this.state.userName)
+
+        }
+    }
+
+    onSearchValueChanged(ev) {
+        this.setState({ userName: ev.target.value })
     }
 
     render() {
         return (
             <div>
-                <input type = "text" className = "searchBoxUsername" onChange = {this.onSearchValueChanged.bind(this)} />
-                <button className = "searchButton" onClick = {this.onSearchClicked.bind(this)}>Search</button>
+                <input type="text" className="searchBoxUsername" onChange={this.onSearchValueChanged.bind(this)} onKeyPress={this.onKeyPress.bind(this)} />
+                <button className="searchButton" onClick={this.onSearchClicked.bind(this)}>Search</button>
             </div>
         );
     }
